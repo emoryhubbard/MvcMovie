c998a2b0936748352ee781107f47c69d20450f95
emoryhubbard <edhthe4th@gmail.com>
2023-07-01 16:44:32 -0400

    Add project files.

---
+++ MvcMovie/Migrations/20230701201100_Image.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class Image : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Movie",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Movie");
+        }
+    }
+}
---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models;
+
+public static class SeedData {
+    public static void Initialize(IServiceProvider serviceProvider) {
+        using (var context = new MvcMovieContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMovieContext>>())) {
+            // Look for any movies.
+            if (context.Movie.Any()) {
+                return;   // DB has been seeded
+            }
+            context.Movie.AddRange(
+                new Movie {
+                    Title = "How Rare a Possession",
+                    ReleaseDate = DateTime.Parse("1987-11-8"),
+                    Genre = "Documentary",
+                    Rating = "Not Rated",
+                    Price = 7.99M,
+                    Image = "how-rare-a-possession.jpg"
+                },
+                new Movie {
+                    Title = "Joseph Smith: The Prophet of the Restoration",
+                    ReleaseDate = DateTime.Parse("2005-12-17"),
+                    Genre = "Documentary",
+                    Rating = "Not Rated",
+                    Price = 8.99M,
+                    Image = "joseph-smith-prophet-of-the-restoration.jpg"
+                },
+                new Movie {
+                    Title = "Meet the Mormons",
+                    ReleaseDate = DateTime.Parse("2014-10-10"),
+                    Genre = "Documentary",
+                    Rating = "PG",
+                    Price = 9.99M,
+                    Image = "meet-the-mormons.jpg"
+                },
+                new Movie {
+                    Title = "The Other Side of Heaven 2",
+                    ReleaseDate = DateTime.Parse("2019-6-28"),
+                    Genre = "Drama",
+                    Rating = "PG-13",
+                    Price = 12.99M,
+                    Image = "the-other-side-of-heaven-2.jpg"
+                },
+                new Movie {
+                    Title = "The Other Side of Heaven",
+                    ReleaseDate = DateTime.Parse("2001-10-14"),
+                    Genre = "Drama",
+                    Rating = "PG",
+                    Price = 7.99M,
+                    Image = "the-other-side-of-heaven.jpg"
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
